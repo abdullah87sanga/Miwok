a24b1d1d0a88a00fd2a6903d91b67209eb1ffbfa
abdullah <abdullah.3del@gmail.com>
2018-05-02 01:08:09 +0200

    images and visual polish

--- app/src/main/java/com/example/android/miwok/WordAdapter.java
+++ app/src/main/java/com/example/android/miwok/WordAdapter.java
@@ -3,19 +3,23 @@ package com.example.android.miwok;
 import android.app.Activity;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Objects;
 
 class WordAdapter extends ArrayAdapter<Word> {
-    public WordAdapter(Activity context, ArrayList<Word> words) {
+    private int mColorResourceId;
+    public WordAdapter(Activity context, ArrayList<Word> words,int colorResourceId) {
         super(context, 0, words);
+       this.mColorResourceId=colorResourceId;
     }
 
     @NonNull
@@ -31,6 +35,11 @@ class WordAdapter extends ArrayAdapter<Word> {
 
         // Get the {@link AndroidFlavor} object located at this position in the list
         Word currentWord = getItem(position);
+        LinearLayout linearLayout = (LinearLayout) listItemView.findViewById(R.id.linearLayout);
+        int color = ContextCompat.getColor(getContext(), mColorResourceId);
+        linearLayout.setBackgroundColor(color);
+        // Get the version number from the current AndroidFlavor object and
+        // set this text on the number TextView
 
         // Find the TextView in the list_item.xml layout with the ID version_name
         TextView miwokTextView = (TextView) listItemView.findViewById(R.id.miwok_text_view);
